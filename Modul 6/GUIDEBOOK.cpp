7bd559705c26a84d0362565429e400acd7adc2cb
Fauzan Adhim Mubarak <f1d02510113@student.unram.ac.id>
2026-04-23 21:01:57 +0800

    Jurnal Sementara

---
+++ Modul 6/GUIDEBOOK.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int Padi_A, Jagung_B, Gandum_C;
+
+    cout << "Masukkan berat Padi (Kg): " << Padi_A << " Kg" << endl;
+    cin >> Padi_A;
+    cout << "Masukkan berat Jagung (Kg): " << Jagung_B << " Kg" << endl;
+    cin >> Jagung_B;
+    cout << "Masukkan berat Gandum (Kg): " << Gandum_C << " Kg" << endl;
+    cin >> Gandum_C;
+
+    //Distribusi hari ke-1nn
+    int Andi_Padi = Padi_A * (40/100), Budi_Padi = Padi_A * (35/100), Citra_Padi;
+    int Andi_Jagung, Budi_Jagung, Citra_Jagung;
+    int Andi_Gandum, Budi_Gandum, Citra_Gandum;
+
+    Andi_Padi ;
+    Budi_Padi = ;
+    Citra_Padi = Padi_A * (25/100);
+
+    Andi_Jagung = Jagung_B * (30/100);
+    Budi_Jagung = Jagung_B * (50/100);
+    Citra_Jagung = Jagung_B * (20/100);
+    
+    Andi_Gandum = Gandum_C * (25/100);
+    Budi_Gandum = Gandum_C * (25/100);
+    Citra_Gandum = Gandum_C * (50/100);
+
+    //Total hari ke-1
+    int Hasil_Andi, Hasil_Budi, Hasil_Citra;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
